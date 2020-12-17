013de48c7235c3a5ae4b131e53d00d7a46a858b6
raisoturu <anvaqta@student.telkomuniversity.ac.id>
2020-12-17 13:48:14 +0000

    Deploying to production from  @ 0a4ccd5edd9e40ba43580c2f218aee9b87adfb0d 🚀

---
+++ _next/static/nWQokwpwIUavBIDdhcOWS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F9ac62baa638228d52db3.css","static\u002Fchunks\u002Fpages\u002Findex-81c3c4163c14ca7a5c6b.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-b52a0603ab19f28b2fd6.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
